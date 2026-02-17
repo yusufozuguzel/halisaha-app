d053e736273be0b4c22113449e98d92b8096848e
yusuf <yusufozuguzel@gmail.com>
2026-02-17 19:54:16 +0300

    Getx ile temel mimari ve moduler yapi olusturuldu

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'routes/app_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Halisaha App',
+      initialRoute: AppPages.initial,
+      getPages: AppPages.routes,
+    );
+  }
+}

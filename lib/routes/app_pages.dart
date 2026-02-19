d39b3474de9d62f807347ed8741a43cf42726740
ronur1906 <ronur1906@gmail.com>
2026-02-19 16:51:41 +0300

    feat: Home UI tasarımı ve alt menü tamamlandı

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:halisaha_app/modules/home/views/home_view.dart';
 import '../modules/auth/views/auth_view.dart';
 import '../modules/auth/bindings/auth_binding.dart';
 import 'app_routes.dart';
@@ -7,10 +8,10 @@ class AppPages {
   static const initial = Routes.AUTH;
 
   static final routes = [
+    GetPage(name: Routes.AUTH, page: () => AuthView(), binding: AuthBinding()),
     GetPage(
-      name: Routes.AUTH,
-      page: () => AuthView(),
-      binding: AuthBinding(),
+      name: Routes.HOME,
+      page: () => const HomeView(userName: "Ahmet Yılmaz"),
     ),
   ];
 }

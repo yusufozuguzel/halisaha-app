763172a18876cca1d949f6f8c2cca5a8b49ca991
yusuf <yusufozuguzel@gmail.com>
2026-07-20 22:29:52 +0300

    feat: initialize Firebase configuration for Android and multi-platform support

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -57,25 +57,23 @@ class DefaultFirebaseOptions {
     projectId: 'halisaha-app-d9ea1',
     storageBucket: 'halisaha-app-d9ea1.firebasestorage.app',
   );
-
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyAxlWugk_WFHInhHH0vihrahPdYB3e76ow',
     appId: '1:2273283642:ios:207afa0fbd59178fb72f03',
     messagingSenderId: '2273283642',
     projectId: 'halisaha-app-d9ea1',
     storageBucket: 'halisaha-app-d9ea1.firebasestorage.app',
-    androidClientId: '2273283642-7op4af2tj5pi70tif7pqdvcoe536gi0d.apps.googleusercontent.com',
+    androidClientId: '2273283642-6jm196jmlodr6up53vamroigrtl0bphd.apps.googleusercontent.com',
     iosClientId: '2273283642-ke215fauar6ciloq25771n3v8kcjnag9.apps.googleusercontent.com',
     iosBundleId: 'com.depar.app',
   );
-
   static const FirebaseOptions macos = FirebaseOptions(
     apiKey: 'AIzaSyAxlWugk_WFHInhHH0vihrahPdYB3e76ow',
     appId: '1:2273283642:ios:4156208eddc08ea5b72f03',
     messagingSenderId: '2273283642',
     projectId: 'halisaha-app-d9ea1',
     storageBucket: 'halisaha-app-d9ea1.firebasestorage.app',
-    androidClientId: '2273283642-7op4af2tj5pi70tif7pqdvcoe536gi0d.apps.googleusercontent.com',
+    androidClientId: '2273283642-6jm196jmlodr6up53vamroigrtl0bphd.apps.googleusercontent.com',
     iosClientId: '2273283642-n76p6ql1hj8q40jam1963cqalt014cb1.apps.googleusercontent.com',
     iosBundleId: 'com.example.halisahaApp',
   );
@@ -89,5 +87,4 @@ class DefaultFirebaseOptions {
     storageBucket: 'halisaha-app-d9ea1.firebasestorage.app',
     measurementId: 'G-323HMZVN73',
   );
-
 }
